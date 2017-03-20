5e3d3d62aa60bdb06e26e4cc22a8b26d2bb57bc9
stevezhao <steve.zhao@gmail.com>
2017-03-20 19:47:19 -0400

    new

--- app/src/main/java/com/example/android/march20class/MainActivity.java
+++ app/src/main/java/com/example/android/march20class/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.android.march20class;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference personRef = database.getReference("person");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void set(View view) {
+        personRef.setValue(new Person("Steve", 100, true));
+    }
+}
 }
